23115f20add8128a528fdafe0c140b03b10224b8
Kigale <lukas.riedel@gmail.com>
2020-10-01 21:56:45 +0200

    Added intro text and project data, styling

--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -3,7 +3,25 @@
     <div class="coverimage">
       <g-image :src="$page.project.coverImage" />
     </div>
-    <div class="projectContent" v-html="$page.project.content" />
+    <div class="projectContent">
+      <h1 class="project">{{ $page.project.title }}</h1>
+      <p class="intro">{{ $page.project.intro }}</p>
+      <div class="projectDataWrapper" style="margin-bottom: 5rem">
+        <hr />
+        <div class="projectData">
+          <div>
+            <h3>Client</h3>
+            <p>{{ $page.project.company }}</p>
+          </div>
+          <div>
+            <h3>Responsibilities</h3>
+            <p>{{ $page.project.responsibilities }}</p>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div v-html="$page.project.content"></div>
+    </div>
   </Layout>
 </template>
 
@@ -12,13 +30,17 @@ query project($path: String!) {
     project: project(path: $path){
         title
         content
-        coverImage (blur: 5)
+        company
+        coverImage (quality: 90)
+        intro
+        responsibilities
     }
 }
 </page-query>
 
 <script>
 export default {
+  props: ["carddata"],
   metaInfo() {
     return {
       title: this.$page.project.title,
@@ -30,23 +52,62 @@ export default {
 <style lang="scss">
 .intro {
   font-style: italic;
-  margin-top: 2rem;
-  margin-bottom: 3rem;
+  margin-bottom: 5rem;
 }
 .coverimage {
-  width: 100vw;
-  height: 100%;
-  object-fit: cover;
+  img {
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+  }
+  @media (orientation: portrait) {
+    img {
+      height: 50vh;
+    }
+  }
+}
+.projectData {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 2rem;
+  div {
+    p {
+      margin: 0;
+      padding: 0;
+    }
+  }
 }
 .projectContent {
-  background: var(--bg-color);
+  div.projectDataWrapper,
+  p,
+  h1,
+  h2 {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: var(--content-width);
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+}
+figure {
+  margin: 5rem auto;
+  max-width: calc(var(--content-width) + 2 * var(--space));
+  padding-left: 0rem;
+  padding-right: 0rem;
+  figcaption {
+    display: none;
+  }
+}
+blockquote {
   max-width: var(--content-width);
-  margin: -100px auto 0;
-  padding: var(--space);
-  img {
-    width: calc(100% + var(--space) * 2);
-    max-width: none;
-    margin-left: calc(var(--space) * -1);
+  font-size: 1.5rem;
+  padding: 7rem 4rem;
+  margin-left: auto;
+  margin-right: auto;
+  p {
+    margin-top: 1rem;
+    text-align: right;
+    color: var(--subtitle-color);
   }
 }
 </style>
