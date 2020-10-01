78b111b293d61d5f6ba8ce494058f551d1e2e90e
Kigale <lukas.riedel@gmail.com>
2020-10-01 21:55:35 +0200

    Styling updates to header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,20 +3,19 @@
     <header class="header contentBig">
       <strong>
         <div v-if="showNavigation">
-          <g-link to="/" class="nav__back">
-            <back />Back
-          </g-link>
+          <g-link to="/" class="nav__back"> <back />Back </g-link>
         </div>
         <h1 v-else>{{ $static.metadata.siteName }}</h1>
       </strong>
-      <nav class="nav">
+      <nav class="nav" v-if="showNavigation === false">
         <a href="mailto:lukas.riedel@gmail.com" class="nav__link">Email</a>
         <br />
         <a
           href="https://www.linkedin.com/in/lukasriedel/"
           class="nav__link"
           target="_blank"
-        >LinkedIn</a>
+          >LinkedIn</a
+        >
         <br />
         <a href="tel:+46705954749" class="nav__link">+46 70 595 47 49</a>
       </nav>
@@ -51,6 +50,11 @@ query {
   left: 0;
   top: 0;
   right: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 160px;
   strong {
     a {
       font-size: 2.25rem;
@@ -58,21 +62,25 @@ query {
     }
   }
 }
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 160px;
-}
-
 .nav__back {
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-gap: 1rem;
+  grid-column-gap: 1rem;
   svg {
     margin: auto;
+  }
+}
+@media screen and (max-width: 720px) {
+  .header {
+    height: 80px;
+    strong {
+      a {
+        font-size: 1.25rem;
+      }
+    }
+  }
+  .nav__back {
+    grid-column-gap: 0.5rem;
   }
 }
 
