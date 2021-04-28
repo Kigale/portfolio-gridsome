c53ba3cdc0c16bbdff5b03b5430a5e2a48b761af
Kigale <lukas.riedel@gmail.com>
2021-04-28 20:57:44 +0200

    Deleting project layout

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import ProjectLayout from '~/layouts/Project.vue'
 import '~/assets/css/styles.scss'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-  Vue.component('ProjectLayout', ProjectLayout)
 }
