1f9c0e7b81111561f3edf9ca406806e4c5c7f8be
Kigale <lukas.riedel@gmail.com>
2021-04-30 11:47:03 +0200

    Added vue gtag

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 import '~/assets/css/styles.scss'
+import VueGtag from "vue-gtag";
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(VueGtag, {
+    config: { id: "G-L0JE1HKH60" }
+  });
 }
